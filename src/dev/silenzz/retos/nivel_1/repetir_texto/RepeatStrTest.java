27f1db4ec5a67da4df048e837d55b27c16235716
Jonathan Felix <jonafelix24@gmail.com>
2024-02-05 01:28:47 -0700

    tabs por espacios

--- src/dev/silenzz/retos/nivel_1/repetir_texto/RepeatStrTest.java
+++ src/dev/silenzz/retos/nivel_1/repetir_texto/RepeatStrTest.java
@@ -6,34 +6,34 @@ import org.junit.jupiter.api.Test;
 
 class RepeatStrTest {
 
-	@Test 
+	@Test
 	public void test4a() {
-        assertEquals("aaaa", RepeatStr.repeat(4, "a"));
-    }
-	
-    @Test 
-    public void test3Hello() {
-        assertEquals("HelloHelloHello", RepeatStr.repeat(3, "Hello"));
-    }
-    
-    @Test 
-    public void test5empty() {
-        assertEquals("", RepeatStr.repeat(5, ""));
-    }
-    
-    @Test 
-    public void test0kata() {
-        assertEquals("", RepeatStr.repeat(0, "kata"));
-    }
-    
-    @Test 
+		assertEquals("aaaa", RepeatStr.repeat(4, "a"));
+	}
+
+	@Test
+	public void test3Hello() {
+		assertEquals("HelloHelloHello", RepeatStr.repeat(3, "Hello"));
+	}
+
+	@Test
+	public void test5empty() {
+		assertEquals("", RepeatStr.repeat(5, ""));
+	}
+
+	@Test
+	public void test0kata() {
+		assertEquals("", RepeatStr.repeat(0, "kata"));
+	}
+
+	@Test
 	public void test4null() {
-        assertEquals("", RepeatStr.repeat(4, null));
-    }
-    
-    @Test 
+		assertEquals("", RepeatStr.repeat(4, null));
+	}
+
+	@Test
 	public void test1kata() {
-        assertEquals("kata", RepeatStr.repeat(1, "kata"));
-    }
+		assertEquals("kata", RepeatStr.repeat(1, "kata"));
+	}
 
 }
